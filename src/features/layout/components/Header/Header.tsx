d2ed1eb934bdfe57152f410daa417e2beed1bbbd
Victor Sandoval <vicbol10@gmail.com>
2025-10-22 19:09:32 -0500

    fix: fix redirect logo

--- src/features/layout/components/Header/Header.tsx
+++ src/features/layout/components/Header/Header.tsx
@@ -1,15 +1,20 @@
 import './Header.scss';
 import Logo from '@src/assets/svgs/logo.svg?react';
 import IconPhone from '@src/assets/svgs/icon-phone.svg?react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => (
   <div className="c-header">
-    <a href="/"><Logo aria-label='Logotipo de Rimac' /></a>
+    <Link aria-label="Logotipo de Rimac" to={{ pathname: '/' }} replace>
+      <Logo />
+    </Link>
     <div className="c-header__information">
       <p className="c-header__text">¡Compra por este medio!</p>
       <div className="c-header__phone-container">
         <IconPhone aria-hidden="true" />
-        <span className="c-header__phone" aria-label="Teléfono">(01) 411 6001</span>
+        <span className="c-header__phone" aria-label="Teléfono">
+          (01) 411 6001
+        </span>
       </div>
     </div>
   </div>
